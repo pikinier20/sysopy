6e9d4640bf77b0f23ebd2531ec17adb9cd6ff372
Filip Zybala <filip.zybala@gmail.com>
2019-03-09 17:33:25 +0100

    Fixed CMakeLists in zad1

---
+++ zad3/mylib.c
@@ -0,0 +1,69 @@
+#include "mylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+struct wrapped_array* create(int array_size){
+    if(array_size <= 0) return NULL;
+
+    struct wrapped_array *res = malloc(sizeof(struct wrapped_array));
+    res->size = array_size;
+    char** arr = (char**) calloc(array_size,sizeof(char*));
+    res->array = arr;
+    
+    return res;
+}
+
+int add_block_at_index(struct wrapped_array* arr, char* file, int index){
+    char* block = load_file(file);
+    if(block == NULL || arr == NULL || index >= arr->size || index < 0) return -1;
+
+    arr->array[index] = (char*) calloc(strlen(block), sizeof(char));
+    strcpy(arr->array[index], block);
+    return index;
+}
+
+int delete_block_at_index(struct wrapped_array* arr,int index){
+    if(arr == NULL || arr->array[index] == NULL || index < 0 || index >= arr->size ) return -1;
+
+
+    free(arr->array[index]);
+    arr->array[index]=NULL;
+
+    return 0;
+}
+
+char* find_file(char* dir, char* file, char* name_file_temp){
+    if(dir == NULL || file == NULL || name_file_temp == NULL) return NULL;
+    char command[255];
+    strcpy(command, "find ");
+    strcat(command, dir);
+    strcat(command, " -name ");
+    strcat(command, file);
+    strcat(command, " >>");
+    strcat(command, name_file_temp);
+    strcat(command, " 2> error.log");
+
+    system(command);
+
+    return name_file_temp;
+}
+
+char* load_file(char* path){
+    if(path == NULL) return NULL;
+    
+    FILE* filestream = fopen(path,"r");
+    if(filestream == NULL) return NULL; //jezeli plik nie zostal znaleziony to NULL
+
+    fseek(filestream, 0L, SEEK_END);
+    int size = ftell(filestream);
+    if(size > 1024) size = 1024;
+    rewind(filestream);
+    char *res_string = (char*) calloc(size, sizeof(char));
+    fread(res_string, sizeof(char), size, filestream);
+    return res_string;
+}
+
+
+
---
+++ zad3/mylib.h
@@ -0,0 +1,22 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+struct wrapped_array {
+    int size;
+    char **array;
+};
+
+typedef struct wrapped_array wrapped_array;
+
+struct wrapped_array* create(int array_size);
+
+int add_block_at_index(struct wrapped_array* arr, char* file, int index);
+
+int delete_block_at_index(struct wrapped_array* arr,int index);
+
+char* find_file(char* dir, char* file, char* name_file_temp);
+
+char* load_file(char* path);
+
+
+#endif //MYLIB_H
