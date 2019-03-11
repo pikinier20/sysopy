2c50846b54b5c60cd9ad0c3807fde0b055aa13c9
pikinier20 <filip.zybala@gmail.com>
2019-03-11 19:07:40 +0100

    Deleted rubbish

--- zad3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/sysopy/zad3
-# Build directory: /home/user/sysopy/zad3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
